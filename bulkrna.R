170460e6666539adb88ae6dd5404674a2caaad21
fbazzi19 <fbazzi19@gmail.com>
2024-05-21 14:58:38 +0200

    added start of preliminary inspection of the data

--- bulkrna.R
+++ bulkrna.R
@@ -1,9 +1,22 @@
-setwd("C:/Users/meemo/Downloads/Genomics and Transcriptomics/Transcriptomics Exam")
-
-
 #library calls
 library(recount3)
 
 #brain data
-brain <- readRDS("rse_brain.RDS")
+brain <- readRDS("C:/Users/meemo/Downloads/Genomics and Transcriptomics/Transcriptomics Exam/rse_brain.RDS")
 head(brain)
+
+#transform counts
+assays(brain)$counts <- transform_counts(brain)
+brain
+
+#look at what information is contained within colData
+names(colData(brain))
+
+#observe the RIN of each sample
+head(colData(brain)$gtex.smrin)
+#sample sources
+table(colData(brain)$gtex.smtsd)
+#sample sexes
+table(colData(brain)$gtex.sex)
+#sample ages
+table(colData(brain)$gtex.age)
