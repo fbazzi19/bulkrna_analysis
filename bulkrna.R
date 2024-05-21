875def263983d14e7704f66c69628a0b2aa729fe
fbazzi19 <fbazzi19@gmail.com>
2024-05-21 15:31:48 +0200

    take an in depth look at was is contained in the data, and added a todo

--- bulkrna.R
+++ bulkrna.R
@@ -1,7 +1,7 @@
-#library calls
+#----library calls ----
 library(recount3)
 
-#brain data
+#----brain data----
 brain <- readRDS("C:/Users/meemo/Downloads/Genomics and Transcriptomics/Transcriptomics Exam/rse_brain.RDS")
 head(brain)
 
@@ -20,3 +20,36 @@ table(colData(brain)$gtex.smtsd)
 table(colData(brain)$gtex.sex)
 #sample ages
 table(colData(brain)$gtex.age)
+
+#the number of reads in each sample
+head(colData(brain)$"recount_qc.star.number_of_input_reads_both") #both because the sequencing was paired end
+#the percentage of uniquely mapped reads
+head(colData(brain)$'recount_qc.star.uniquely_mapped_reads_%_both')
+
+#plotting the distribution of reads per sample
+inputreads <- colData(brain)$"recount_qc.star.number_of_input_reads_both"
+boxplot(inputreads)
+
+#plotting the percentage of mapped reads
+mapped <- colData(brain)$"recount_qc.star.uniquely_mapped_reads_%_both"
+boxplot(mapped)
+
+#estimated percent of reads coming from ribosomal RNA
+boxplot(colData(brain)$gtex.smrrnart)
+#estimated percent of reads coming from mitochondrial genes
+boxplot(colData(brain)$"recount_qc.aligned_reads%.chrm")
+#rRNA and mitochondrial genes
+plot(colData(brain)$gtex.smrrnart, colData(brain)$"recount_qc.aligned_reads%.chrm")
+
+#look at what information is contained within rowData
+names(rowData(brain))
+
+#types of genes
+table(rowData(brain)$gbkey)
+#genomic coordinates of each gene
+rowRanges(brain)
+#how many genes come from each chromosome
+table(rowRanges(brain)@seqnames) #indicated that non-canonical chromosomes were included
+
+#---- check assigned columns ----
+#todo: make it a function
