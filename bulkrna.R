e816892ad5969222b90dc296d8cf44f4df49b151
fbazzi19 <fbazzi19@gmail.com>
2024-05-21 14:05:23 +0200

    initial commit

---
+++ bulkrna.R
@@ -0,0 +1,9 @@
+setwd("C:/Users/meemo/Downloads/Genomics and Transcriptomics/Transcriptomics Exam")
+
+
+#library calls
+library(recount3)
+
+#brain data
+brain <- readRDS("rse_brain.RDS")
+head(brain)
