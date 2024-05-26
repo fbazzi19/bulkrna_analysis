87286dbf86a6f09744e26cbfa170c68d726b251c
fbazzi19 <fbazzi19@gmail.com>
2024-05-26 21:57:08 +0200

    Finished DGE object and linear model, did some analysis

--- bulkrna.R
+++ bulkrna.R
@@ -17,11 +17,12 @@ head(liver)
 assays(brain)$counts <- transform_counts(brain)
 assays(blood)$counts <- transform_counts(blood)
 assays(liver)$counts <- transform_counts(liver)
-
-
-#---- Data Overview (Brain only) ----
 # Compute TPMs
 assays(brain)$TPM <- recount::getTPM(brain)
+assays(blood)$TPM <- recount::getTPM(blood)
+assays(liver)$TPM <- recount::getTPM(liver)
+
+#---- Data Overview (Brain only) ----
 
 #look at what information is contained within colData
 names(colData(brain))
@@ -150,3 +151,151 @@ colnames(x) <- c("Brain30", "Brain31","Brain33",
 
 #----DGE Object----
 y <- DGEList(counts = x)
+#define how the replicates are grouped
+group <- as.factor(c("Brain","Brain","Brain",
+                     "Blood","Blood","Blood",
+                     "Liver","Liver","Liver"))
+
+y$samples$group <- group
+
+#add information to the samples regarding the quality
+y$samples$rin <- as.factor(c(colData(brain[,brain_cols])$gtex.smrin,
+                             colData(blood[,blood_cols])$gtex.smrin,
+                             colData(liver[,liver_cols])$gtex.smrin))
+
+y$samples$slice <- as.factor(c(colData(brain[,brain_cols])$gtex.smtsd,
+                               colData(blood[,blood_cols])$gtex.smtsd,
+                               colData(liver[,liver_cols])$gtex.smtsd))
+
+y$samples$sex <- as.factor(c(colData(brain[,brain_cols])$gtex.sex,
+                             colData(blood[,blood_cols])$gtex.sex,
+                             colData(liver[,liver_cols])$gtex.sex))
+
+y$samples$age <- as.factor(c(colData(brain[,brain_cols])$gtex.age,
+                             colData(blood[,blood_cols])$gtex.age,
+                             colData(liver[,liver_cols])$gtex.age))
+
+y$samples$rRNA <- as.factor(c(colData(brain[,brain_cols])$gtex.smrrnart,
+                              colData(blood[,blood_cols])$gtex.smrrnart,
+                              colData(liver[,liver_cols])$gtex.smrrnart))
+
+y$samples$mapped <- as.factor(c(colData(brain[,brain_cols])$"recount_qc.star.uniquely_mapped_reads_%_both", 
+                                colData(blood[,blood_cols])$"recount_qc.star.uniquely_mapped_reads_%_both",
+                                colData(liver[,liver_cols])$"recount_qc.star.uniquely_mapped_reads_%_both"))
+
+y$samples$chrm <- as.factor(c(colData(brain[,brain_cols])$"recount_qc.aligned_reads%.chrm", 
+                              colData(blood[,blood_cols])$"recount_qc.aligned_reads%.chrm",
+                              colData(liver[,liver_cols])$"recount_qc.aligned_reads%.chrm"))
+y
+
+#genes with zero counts
+table(rowSums(y$counts==0)==9)
+
+#remove genes with zero or low expression
+keep.exprs <- filterByExpr(y, group=group)
+y <- y[keep.exprs,, keep.lib.sizes=FALSE]
+dim(y)
+
+#----Normalization----
+#log cpm before normalization
+logcpm_before <- cpm(y, log=TRUE)
+#normalization
+y <- calcNormFactors(y, method = "TMM")
+#log cpm after normalization
+logcpm_after <- cpm(y, log=TRUE)
+#boxplots
+#TODO: make pretty
+boxplot(logcpm_before, notch=T)
+boxplot(logcpm_after, notch=T)
+
+#----Linear Model----
+design <- model.matrix(~0+group, data=y$samples)
+colnames(design) <- levels(y$samples$group)
+design
+
+#visualize how samples cluster together
+logcpm <- cpm(y, log=TRUE)
+plotMDS(logcpm, labels=group)
+#label the samples by rRNA
+plotMDS(logcpm, labels=y$samples$rRNA)
+#label by percent of aligned reads
+plotMDS(logcpm, labels=y$samples$chrm)
+#label by age
+plotMDS(logcpm, labels=y$samples$age)
+
+#plot biological coefficient of variation
+y <- estimateDisp(y, design)
+plotBCV(y)
+
+#fit linear model
+fit <- glmQLFit(y, design)
+#brain (top) vs blood (bottom)
+qlfBBL <- glmQLFTest(fit, contrast=c(-1,1,0))
+#liver (top) vs blood (bottom)
+qlfLBL <- glmQLFTest(fit, contrast=c(-1,0,1))
+#liver (top) vs brain (bottom)
+qlfLB <- glmQLFTest(fit, contrast=c(0,-1,1))
+
+qlfBBL
+
+#DE genes, sorted by p-value, with the FDR added
+topTags(qlfBBL, n=10,adjust.method = "BH", sort.by = "PValue")
+topTags(qlfLBL, n=10,adjust.method = "BH", sort.by = "PValue")
+topTags(qlfLB, n=10,adjust.method = "BH", sort.by = "PValue")
+
+#get the whole table and write to a file
+resultsBBL <- topTags(qlfBBL, n = 10000000, adjust.method = "BH", sort.by = "PValue", p.value = 1)
+resultsLBL <- topTags(qlfLBL, n = 10000000, adjust.method = "BH", sort.by = "PValue", p.value = 1)
+resultsLB <- topTags(qlfLB, n = 10000000, adjust.method = "BH", sort.by = "PValue", p.value = 1)
+write.table(resultsBBL, "resultsBBL.txt")
+write.table(resultsLBL, "resultsLBL.txt")
+write.table(resultsLB, "resultsLB.txt")
+
+#summary of DE genes
+summary(decideTests(qlfBBL, p.value=0.05, adjust.method = "BH", lfc=0)) #can adjust p value and log fold change
+summary(decideTests(qlfLBL, p.value=0.05, adjust.method = "BH", lfc=0))
+summary(decideTests(qlfLB, p.value=0.05, adjust.method = "BH", lfc=0))
+
+#find genes that are upregulated 1v3, ie, upregulated in one compared to both
+#first find row names where it is upregulated against one 
+brain_up_idx <- which(topTags(qlfBBL, n = 10000000, adjust.method = "BH", 
+                              sort.by = "PValue", p.value = 0.01)[["table"]]$logFC>0)
+brain_up_genes <- row.names(topTags(qlfBBL, n = 10000000, adjust.method = "BH", 
+                                    sort.by = "PValue", p.value = 0.01)[["table"]][brain_up_idx,])
+#find the rows where it is upregulated against the other
+brain_up_idx <- which(topTags(qlfLB, n = 10000000, adjust.method = "BH", 
+                              sort.by = "PValue", p.value = 0.01)[["table"]]$logFC<0)
+#perform the intersection of the two
+brain_up_genes <- intersect(brain_up_genes, 
+                            row.names(topTags(qlfLB, n = 10000000, adjust.method = "BH", sort.by = "PValue", p.value = 0.01)[["table"]][brain_up_idx,]))
+
+#repeat for blood and liver
+blood_up_idx <- which(topTags(qlfBBL, n = 10000000, adjust.method = "BH", 
+                              sort.by = "PValue", p.value = 0.01)[["table"]]$logFC<0)
+blood_up_genes <- row.names(topTags(qlfBBL, n = 10000000, adjust.method = "BH", 
+                                    sort.by = "PValue", p.value = 0.01)[["table"]][blood_up_idx,])
+#find the rows where it is upregulated against the other
+blood_up_idx <- which(topTags(qlfLBL, n = 10000000, adjust.method = "BH", 
+                              sort.by = "PValue", p.value = 0.01)[["table"]]$logFC<0)
+#perform the intersection of the two
+blood_up_genes <- intersect(blood_up_genes, 
+                            row.names(topTags(qlfLBL, n = 10000000, adjust.method = "BH", sort.by = "PValue", p.value = 0.01)[["table"]][blood_up_idx,]))
+
+liver_up_idx <- which(topTags(qlfLBL, n = 10000000, adjust.method = "BH", 
+                              sort.by = "PValue", p.value = 0.01)[["table"]]$logFC>0)
+liver_up_genes <- row.names(topTags(qlfLBL, n = 10000000, adjust.method = "BH", 
+                                    sort.by = "PValue", p.value = 0.01)[["table"]][liver_up_idx,])
+#find the rows where it is upregulated against the other
+liver_up_idx <- which(topTags(qlfLB, n = 10000000, adjust.method = "BH", 
+                              sort.by = "PValue", p.value = 0.01)[["table"]]$logFC>0)
+#perform the intersection of the two
+liver_up_genes <- intersect(liver_up_genes, 
+                            row.names(topTags(qlfLB, n = 10000000, adjust.method = "BH", sort.by = "PValue", p.value = 0.01)[["table"]][liver_up_idx,]))
+
+#check the expression across all samples, not just my assigned ones
+which(rowData(brain)$gene_name == "UGT1A1") #gene found in liver cells
+boxplot(assays(brain)$TPM[20205,],assays(blood)$TPM[20205,], assays(liver)$TPM[20205,], outline=F )
+#TODO: use a statistical test to prove this expression across all samples is significant
+wilcox.test(assays(brain)$TPM[20205,], assays(blood)$TPM[20205,], alternative='two.sided',exact = F,correct = T)
+wilcox.test(assays(brain)$TPM[20205,], assays(liver)$TPM[20205,], alternative='two.sided',exact = F,correct = T)
+wilcox.test(assays(blood)$TPM[20205,], assays(liver)$TPM[20205,], alternative='two.sided',exact = F,correct = T)
