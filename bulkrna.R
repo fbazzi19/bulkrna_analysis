526b82c14a22303ba7229a387ac9f14d302906a0
fbazzi19 <fbazzi19@gmail.com>
2024-05-26 19:21:54 +0200

    filter for potential bias, get counts from assigned samples, and start with DGE object

--- bulkrna.R
+++ bulkrna.R
@@ -1,13 +1,27 @@
-#----library calls ----
-library(recount3)
+#remember to set the working directory accordingly!
 
-#----brain data----
-brain <- readRDS("C:/Users/meemo/Downloads/Genomics and Transcriptomics/Transcriptomics Exam/rse_brain.RDS")
+#----library calls ----
+library(recount)
+library(recount3)
+library(edgeR)
+
+#----load in and transform counts data----
+brain <- readRDS("rse_brain.RDS")
 head(brain)
+blood <- readRDS("rse_blood.RDS")
+head(blood)
+liver <- readRDS("rse_liver.RDS")
+head(liver)
 
 #transform counts
 assays(brain)$counts <- transform_counts(brain)
-brain
+assays(blood)$counts <- transform_counts(blood)
+assays(liver)$counts <- transform_counts(liver)
+
+
+#---- Data Overview (Brain only) ----
+# Compute TPMs
+assays(brain)$TPM <- recount::getTPM(brain)
 
 #look at what information is contained within colData
 names(colData(brain))
@@ -49,7 +63,90 @@ table(rowData(brain)$gbkey)
 #genomic coordinates of each gene
 rowRanges(brain)
 #how many genes come from each chromosome
-table(rowRanges(brain)@seqnames) #indicated that non-canonical chromosomes were included
+table(rowRanges(brain)@seqnames) #indicates that non-canonical chromosomes were included
+
+
+
+#---- Overall Quality Plots ----
+#TODO: make pretty
+#brain
+boxplot(colData(brain)$gtex.smrin)
+boxplot(colData(brain)$gtex.smrrnart)
+boxplot(colData(brain)$"recount_qc.star.uniquely_mapped_reads_%_both")
+#blood
+boxplot(colData(blood)$gtex.smrin)
+boxplot(colData(blood)$gtex.smrrnart)
+boxplot(colData(blood)$"recount_qc.star.uniquely_mapped_reads_%_both")
+#liver
+boxplot(colData(liver)$gtex.smrin)
+boxplot(colData(liver)$gtex.smrrnart)
+boxplot(colData(liver)$"recount_qc.star.uniquely_mapped_reads_%_both")
+
+#----Clean up factors causing bias----
+#function to clean up factors potentially causing bias
+filter_data <- function(rse){
+  #challenge 1a and 2: remove genes annotated on mitochondrial DNA
+  #and keep genes only on canonical chromosomes
+  canonchr <- c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8",
+                "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15",
+                "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22",
+                "chrX", "chrY")
+  rse<- rse[rowRanges(rse)@seqnames %in% canonchr]
+  
+  #challenge 1b and 1c: remove pseudogenes and rRNA genes
+  rse<- rse[((rowData(rse)$gbkey!="Gene" & rowData(rse)$gbkey!="rRNA") | is.na(rowData(rse)$gbkey))]
+  
+  #challenge 1d: remove genes with annotated length <200
+  rse<- rse[rowData(rse)$bp_length>=200]
+  
+  return(rse)
+}
+
+brain <- filter_data(brain)
+blood <- filter_data(blood)
+liver <- filter_data(liver)
+
 
 #---- check assigned columns ----
-#todo: make it a function
+#This functions starts at a given column and checks if that sample meets
+#the required thresholds. It continues iteratively until it finds 3 samples.
+get_my_samples <- function(rse, start){
+  cols <- c()
+  while (length(cols)<3){
+    if (colData(rse)$gtex.smrin[start]>6 & colData(rse)$gtex.smrrnart[start]<0.1 & colData(rse)$"recount_qc.star.uniquely_mapped_reads_%_both"[start]>85){
+      cols <- c(cols, start)
+      print(paste0("Sample ", start, " has RIN ", colData(rse)$gtex.smrin[start]))
+      print(paste0("Sample ", start, " has rRNA fraction ", colData(rse)$gtex.smrrnart[start]))
+      print(paste0("Sample ", start, " has % mapped reads ", colData(rse)$"recount_qc.star.uniquely_mapped_reads_%_both"[start]))
+    }
+    start <- start+1
+  }
+  return (cols)
+}
+
+#call the function for each tissue type
+brain_cols <- get_my_samples(brain, 30)
+blood_cols <- get_my_samples(blood, 30)
+liver_cols <- get_my_samples(liver, 30)
+
+
+#---- Set up count table for edger ----
+#count tables of selected columns
+counts_brain_selected <- assays(brain[,brain_cols])$counts
+counts_blood_selected <- assays(blood[,blood_cols])$counts
+counts_liver_selected <- assays(liver[,liver_cols])$counts
+
+#define the row names as the official gene names
+rownames(counts_brain_selected) <- rowData(brain)$gene_name
+rownames(counts_blood_selected) <- rowData(blood)$gene_name
+rownames(counts_liver_selected) <- rowData(liver)$gene_name
+
+#bind the count tables together
+x <- cbind(counts_brain_selected,counts_blood_selected,counts_liver_selected)
+#assign the column names
+colnames(x) <- c("Brain30", "Brain31","Brain33",
+                 "Blood30", "Blood31","Blood32",
+                 "Liver31","Liver32","Liver33")
+
+#----DGE Object----
+y <- DGEList(counts = x)
